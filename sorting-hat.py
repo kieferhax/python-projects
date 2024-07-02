2976be977b98871ff8d0e9bfaee624b223a5ca63
Kiefer Land <kland7@wgu.edu>
2024-07-02 12:25:33 -0700

    create sorting-hat.py

---
+++ sorting-hat.py
@@ -0,0 +1,4 @@
+# The Sorting Hat is a magical talking hat at Hogwarts School of Witchcraft and Wizardry.  The hat decides which of the four "Houses" each first-year student goes to:
+# Gryffindor, Ravenclaw, Hufflepuff, Slytherin
+
+# Write a program that asks the user some questions with the int() and input() functions:
