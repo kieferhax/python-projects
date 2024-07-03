a1404e3d8b9864d7f4edbefe12f06e6c2c9fc80f
Kiefer Land <kland7@wgu.edu>
2024-07-03 09:20:59 -0700

    create letter.py

---
+++ letter.py
@@ -0,0 +1,24 @@
+# In this exercise, you will write a letter to your future self...in Python
+# Take a moment to think about what you hope to achieve on this journey
+# Use print() to output:
+# Today's date
+# How you are feeling right now
+# What you want to accomplish by learning to code
+# A little message to your older, wiser, and programmer self.
+# Your favorite emoji to spice thiings up!
+
+print(' ------------------------------------------------- ')
+print('|                              Kiefer Taylor Land |')
+print('|                               Wed. July 3, 2024 |')
+print('|                                                 |')
+print('|Dear Kiefer 🇯🇵                                   |')
+print('|                                                 |')
+print('|I hope this message finds you well.              |')
+print('|                                                 |')
+print('|I am feeling well and hopeful for your future.   |')
+print('|                                                 |')
+print('|You will create tools to empower humanity.       |')
+print('|                                                 |')
+print('|Keep working hard every day and you will make it.|')
+print('|                                                 |')
+print(' ------------------------------------------------- ')
