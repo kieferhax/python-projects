f221c38b7e3105303d6f68fa0938bcbcec7e8fdf
Kiefer Land <kland7@wgu.edu>
2024-07-03 08:49:25 -0700

    create initials.py

---
+++ initials.py
@@ -0,0 +1,13 @@
+# Create an initials.py program that displays your initals in block letters.
+# First, start the program with a fun fact about yourself
+print('Hello, my name is Kiefer!')
+print('Fun fact: I was born in Japan')
+print('My initials in block letters:')
+# Create the block letters with your initials below
+print('K  K L')
+print('K K  L')
+print('KK   L')
+print('K    L')
+print('KK   L')
+print('K K  L')
+print('K  K LLLLL')
