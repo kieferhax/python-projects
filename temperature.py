1306235d5bff73feea5c061f428c911104748502
Kiefer Land <kland7@wgu.edu>
2024-07-05 11:36:19 -0700

    create temperature.py

---
+++ temperature.py
@@ -0,0 +1,10 @@
+# Create a temperature.py program that converts a temperature from Fahrenheit (°F) to Celsius (°C).
+# Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+tempf = 86
+
+# Use the following formula to convert Fahrenheit to Celsius
+# Celsius = (Fahrenheit - 32) / 1.8
+tempc = (tempf - 32) /1.8
+
+# Print out the converted temperature. 🌡️
+print(f'The current temperature in Brooklyn, NY is: {tempc} degrees Celsius.')
