55579bc735de9aac11c426539b1d8b1c61f3579f
Kiefer Land <kland7@wgu.edu>
2024-07-06 06:55:59 -0700

    update coin-flip.py

--- coin-flip.py
+++ coin-flip.py
@@ -1,3 +1,7 @@
+# Simulate a coin toss
+# 50% of the time, it's Head's
+# 50% of the time, it's Tail's
+
 import random
 
 num = random.randint(0,1) # Generates a random number that's either 0 or 1
@@ -6,4 +10,3 @@ if num > 0.5:
   print('Heads')
 else:
   print('Tails')
-
