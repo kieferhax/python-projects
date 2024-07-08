9fb755f8681c9a67c07abdefebea209edbaf156c
Kiefer Land <kland7@wgu.edu>
2024-07-08 10:50:14 -0700

    create 99-bottles.py

---
+++ 99-bottles.py
@@ -0,0 +1,9 @@
+# "99 Bottles of Beer" is an old song that annoying kids, oops I mean everyone, sang on road trips to pass the time.
+# Create a 99_bottles.py program and use a for loop and a range() function to print out all the verses of "99 Bottles of Beer."
+# And don't forget to use string interpolation!
+
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print(f'Take one down, pass it around')
+  print(f'{i-1} bottles of beer on the wall')
