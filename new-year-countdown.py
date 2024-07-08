42c958ed1958a78319f93d282022fffbbc5bbd75
Kiefer Land <kland7@wgu.edu>
2024-07-08 12:46:05 -0700

    create new-year-countdown.py

---
+++ new-year-countdown.py
@@ -0,0 +1,6 @@
+# Ring in the New Year! A New Year's Eve party doesn't feel complete without a countdown from 10 to 0.
+# Use a for loop and the range() function for a countdown.
+# Counting down from 10 to 1 on each line and then a Happy New Year message at the end.
+for i in range(10, 0, -1):
+  print(i)
+print('Happy New Year!')
