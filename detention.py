a7e6cc3ea6c3cfdf644d380b50ad93db05a1d45e
Kiefer Land <kland7@wgu.edu>
2024-07-08 10:40:39 -0700

    update guess.py

---
+++ detention.py
@@ -0,0 +1,5 @@
+# Suppose you got detention and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.
+
+# Create a detention.py program that does this using code.
+
+# 
