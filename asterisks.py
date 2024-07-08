7c341379c16b171e2b1b75862db4c21d49299158
Kiefer Land <kland7@wgu.edu>
2024-07-08 13:15:53 -0700

    create asterisk.py

---
+++ asterisks.py
@@ -0,0 +1,4 @@
+# Create a program that uses a for loop and range() function to display the following pattern in the terminal using a bunch of asterisks *
+# Make it 25 rows
+for i in range(25):
+  print(i * '*')
