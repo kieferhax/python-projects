b40a858db0dbb8e8b87331fefa9586f40b1a0f2a
Kiefer Land <kland7@wgu.edu>
2024-07-08 12:57:54 -0700

    create are-we-there-yet.py

---
+++ are-we-there-yet.py
@@ -0,0 +1,6 @@
+# “Are we there yet?” is a phrase that has existed for as long as there are children, vehicles, and road trips.
+# First, ask the user “Are we there yet?” using the input() function and store it in an answer variable.
+answer = input('Are we there yet? ')
+# Then, use a while loop that asks the user “Are we there yet?” over and over again until they respond with a “Yes”.
+while answer != 'yes':
+  answer = input('Are we there yet? ')
