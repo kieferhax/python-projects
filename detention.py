042838fba596c8f911d2f5c4ad512d108fb71cda
Kiefer Land <kland7@wgu.edu>
2024-07-08 10:43:01 -0700

    create detention.py

--- detention.py
+++ detention.py
@@ -1,5 +1,4 @@
 # Suppose you got detention and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.
-
 # Create a detention.py program that does this using code.
-
-# 
+for i in range(101):
+  print("I will not use SnapChat in class.")
