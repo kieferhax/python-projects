8f4da4d44ea68275e8a9d91f7bd82de2185b2ef6
Kiefer Land <kland7@wgu.edu>
2024-07-09 08:45:54 -0700

    create terminal-game.py

---
+++ terminal-game.py
@@ -0,0 +1,30 @@
+# Create a terminal_game.py that walks the player through a captivating mini-game adventure! In each step of the adventure, the player should be presented with 2 or more options on where to go next.
+
+# A space-based adventure of a crew of curious individuals exploring an unknown galaxy.
+
+# Star Wars themed intro
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
+print('|                                                                                       |')
+print('|                                  SPACE ADVENTURES                                     |')
+print('|                                                                                       |')
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
+print('|                                                                                       |')
+print('| A not so very long time ago, in a not so very far away galaxy..                       |')
+print('|                                                                                       |')
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
+print('|                                                                                       |')
+print('| Among the distant stars, a daring saga unfolds...                                     |')
+print('|                                                                                       |')
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
+print('|                                                                                       |')
+print('| Navigate cosmic battlegrounds...                                                      |')
+print('|                                                                                       |')
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
+print('|                                                                                       |')
+print('| Meet mysterious alien civilizations...                                                |')
+print('|                                                                                       |')
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
+print('|                                                                                       |')
+print('| Unite against a tyrannical empire threatening to plunge the galaxy into darkness...   |')
+print('|                                                                                       |')
+print('| * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * |')
