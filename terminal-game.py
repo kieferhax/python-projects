bf441861a01f691e342ba0b4bc0e77e30dda2423
Kiefer Land <kland7@wgu.edu>
2024-07-10 12:18:07 -0700

    update terminal-game.py

--- terminal-game.py
+++ terminal-game.py
@@ -3,23 +3,43 @@
 # A space-based adventure of a crew of curious individuals exploring an unknown galaxy.
 
 # Star Wars themed intro
-print('* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *')
-print('                                 SPACE ADVENTURES                                    ')
-print('* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *')
-print('                                                                                     ')
-print('A not so very long time ago, in a not so very far away galaxy..                      ')
-print('Among the distant stars, a daring saga unfolds...                                    ')
-print('Navigate cosmic battlegrounds...                                                     ')
-print('Meet mysterious alien civilizations...                                               ')
-print('Unite against a tyrannical empire threatening to plunge the galaxy into darkness...  ')
-print('                                                                                     ')
-print('* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *')
-print('                                                                                     ')
+from time import time
+
+
+print('* * * * * * * * * * * * * * * * * *')
+print('         SPACE ADVENTURES          ')
+print('* * * * * * * * * * * * * * * * * *')
+print('')
+print('A not so very long time ago, in a not so very far away galaxy...')
+print('Among the distant stars, a daring saga unfolds...')
+print('Navigate cosmic battlegrounds...')
+print('Meet mysterious alien civilizations...')
+print('Unite against a tyrannical empire threatening to plunge the galaxy into darkness...')
+print('')
+print('* * * * * * * * * * * * * * * * * *')
+print('')
+# Get player name, rank, and location
 name = input('Enter your name, cadet: ')
 rank = input('Enter your rank: ')
 galaxy = input('Enter your home galaxy: ')
 print('                                                                                     ')
 print(f'Welcome aboard {rank} {name} from the {galaxy} Galaxy. Lets begin our mission...')
-print('                                                                                     ')
-print('* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *')
-print('                                                                                     ')
+print('')
+# Epic 1
+print('Epic 1: The Initial Encounter...')
+print('')
+print('Your spaceship encounters a mysterious alien vessel in deep space...')
+print('A: Approach the mysterious spaceship')
+print('B: Hide behind an asteroid and observe.')
+# Scene1
+scene1 = input('Choose your fate: ')
+if scene1 == a or scene1 = A:
+  print('As your spaceship inches closer, the alien vessel’s lights flicker to life, revealing intricate designs and unfamiliar symbols')
+  print('Suddenly, a holographic message projects into your cockpit, welcoming you aboard.')
+  print('The aliens appear friendly and invite you to dock for a meeting.')
+elif scene1 == 'b' or 'B':
+  print('You skillfully maneuver behind a nearby asteroid, observing the alien ship’s movements.')
+  print('You notice a pattern in their patrol routes and detect a weak point in their defenses.')
+  print('This information could be invaluable for future encounters.')
+else:
+  input('That is not an option, please try again.')
