8d2721e3d90a3b6ed60668707910be4502e74548
Kiefer Land <kland7@wgu.edu>
2024-07-12 13:00:18 -0700

    create inventory.py

---
+++ inventory.py
@@ -0,0 +1,5 @@
+# Suppose the holidays are around the corner, and the North Pole elves need your help to have thousands of LEGO toys packed up and ready for shipping.
+# Each item in the list is the quantity of a colored part for a LEGO toy.
+# Can you figure out the following information using built-in list functions?
+# Which LEGO part are the elves running low on? Use min() to find out.
+# Is there a LEGO part that the elves are overstocking? Use max() to find out.
