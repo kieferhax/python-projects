030249cca125bbca0153a14766d4417f38a65ee0
Kiefer Land <kland7@wgu.edu>
2024-07-12 12:57:38 -0700

    create grocery.py & todo.py

---
+++ todo.py
@@ -0,0 +1,19 @@
+# Create a todo.py program that will define a todo list that contains a list of items
+todo = ['🏦 Get quarters.',
+        '🧺 Do laundry.',
+        '🌳 Take a walk.',
+        '💈 Get a haircut.',
+        '🍵 Make some tea.',
+        '💻 Complete Lists chapter.',
+        '💖 Call mom.',
+        '📺 Watch My Hero Academia.']
+
+# Print the first item and the second item. What did you get?
+print(todo[0])
+print(todo[1])
+
+# Next, use slicing to print the third, fourth, and fifth items.
+print(todo[2 : 5])
+
+# Try printing out the item at index 9 to see the IndexError before moving on.
+print(todo[9])
