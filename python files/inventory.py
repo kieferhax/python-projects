572098ccfdcf072af2f1b0a9c480779a3ab07f99
Kiefer Land <kland7@wgu.edu>
2024-07-13 09:50:04 -0700

    update inventory.py

--- python files/inventory.py
+++ python files/inventory.py
@@ -1,5 +1,11 @@
 # Suppose the holidays are around the corner, and the North Pole elves need your help to have thousands of LEGO toys packed up and ready for shipping.
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+
 # Each item in the list is the quantity of a colored part for a LEGO toy.
+
 # Can you figure out the following information using built-in list functions?
+
 # Which LEGO part are the elves running low on? Use min() to find out.
+print(min(lego_parts))
 # Is there a LEGO part that the elves are overstocking? Use max() to find out.
+print(max(lego_parts))
