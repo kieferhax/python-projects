3e8c24b0e274b5e3a923c1e874b14138153dc40d
Kiefer Land <kland7@wgu.edu>
2024-07-13 18:47:20 -0700

    update mixtape.py

---
+++ python files/bucket-list.py
@@ -0,0 +1,20 @@
+# A bucket list is a number of experiences or achievements that a person hopes to have or accomplish during their lifetime
+
+# Create a bucket_list.py program with your own unique bucket list.
+
+# First, create a things_to_do list, and add things you want to do in your lifetime.
+
+things_to_do = [
+  '⛰️ Hike the Pacific Crest Trail.',
+  '🏡 Build an A-frame house and raise some goats.',
+  '🌏 Live somewhere in Asia for a year.',
+  '🎸 Release an album.',
+  '📝 Write a book.',
+  '🏆 Reach 100k subscribers on YouTube.',
+  '🚐 Road trip.',
+  '🍳 Open a diner.',
+  '👴🏻 Grow old with no regrets.'
+]
+
+for i in things_to_do:
+  print(i)
