c0e5dd1397274644962207722945a855f68c4eee
Kiefer Land <kland7@wgu.edu>
2024-07-14 11:09:49 -0700

    create wishlisht.py

---
+++ python files/wishlist.py
@@ -0,0 +1,11 @@
+# Many folks, especially around the holidays, carry a wishlist of things they've always wanted. 
+# Use Python lists to make your own wishlist!
+# Create a list called wishlist and add at least three items to the list. 
+# Each item should be a string of something you've always wanted.
+wishlish = [
+  'Healthy',
+  'Wealthy',
+  'Wise'
+]
+# Print the wishlist to the terminal to see all your wishes in one place!
+print(wishlish)
