889007820528e1e509a5c8819a50e6343c778bce
Kiefer Land <kland7@wgu.edu>
2024-07-13 18:44:40 -0700

    create mixtape.py

---
+++ python files/mixtape.py
@@ -0,0 +1,16 @@
+# Back in the good ol' days, a mixtape is a compilation of music, a playlist, recorded onto a cassette tape or a CD.
+
+# Create a program and think of a theme for the songs.
+
+# 💿 Theme: Indie Travel Songs
+
+# Make a list called playlist and throw in some songs.
+playlist = ['Porches - rangerover',
+            'Mount Eerie - You Swan, Go On',
+            'Carolyn Polachek - Look at Me Now',
+            'Pinegrove - Darkness',
+            'LVP UP - Spirit Was',
+            'Mitski - First Love / Late Spring']
+
+for i in playlist:
+  print(i)
