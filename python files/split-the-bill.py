57d123bb380177e15886fddf5256b7df2ace0e05
Kiefer Land <kland7@wgu.edu>
2024-07-15 16:44:13 -0700

    split-the-bill.py

---
+++ python files/split-the-bill.py
@@ -0,0 +1,12 @@
+# Suppose you and three of your friends all went out for a meal together. 
+# The bill comes due, and all of you decide to split the bill evenly.
+bill = [13.99, 28.75, 9.99, 9.99, 6.95, 7.45, 16.45, 16.45]
+
+# Using this bill list, calculate the total amount and then divide it four ways.
+total = 0
+# Initialize the total with zero and loop through the bill list to add everything up.
+for item in bill:
+    print(item)
+
+# With the total, store what each person has to pay in a my_share variable and then print the result to the terminal.
+my_share = None
