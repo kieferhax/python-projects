0b668674871a1445adc93b8e31b9ac30872b5533
Kiefer Land <kland7@wgu.edu>
2024-07-17 09:45:49 -0700

    update dry.py

--- python files/dry.py
+++ python files/dry.py
@@ -5,10 +5,21 @@
 # For the new function, try to read the documentation (😵‍💫) or Google it (👍)!
 # Add a comment for each built-in function to explain how they work.
 
-# print(), input(), str(), float(), range(), round(), reverse(), sum()
-print('This is a string')
-print(str('27'))
-print(int('27'))
-input('What is your favorite number: ')
-float = float(2.0)
-print(float)
+# The print() function outputs specified text or variables to the console or other output devices
+print('The print() function outputs specified text or variables to the console or other output devices')
+
+# The input() function reads a line of text from the user via the keyboard and returns it as a string
+hello = input('The input() function reads a line of text from the user via the keyboard and returns it as a string. Try is out: ')
+print(hello)
+
+# The len() function
+num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(len(num))
+
+# The range() function in Python generates a sequence of integers from a starting value up to, but not including, an ending value, with an optional step size
+print(list(range(5)))
+print(list(range(1, 6)))
+print(list(range(0, 10, 2)))
+
+# the help() function in Python is used to get help information about a specified object, module, function, class, or keyword.
+# help(print)
