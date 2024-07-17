ce4af099c5e85f24f0e28f84fc289bbb8e3b41f5
Kiefer Land <kland7@wgu.edu>
2024-07-17 09:30:40 -0700

    dry.py

---
+++ python files/dry.py
@@ -0,0 +1,14 @@
+# Create a dry.py program and check out the complete list of built-in functions:
+# Find 4 built-in functions that we have used previously in the course.
+# Pick 1 built-in function that we have not seen before.
+# Use each of these once in the program.
+# For the new function, try to read the documentation (😵‍💫) or Google it (👍)!
+# Add a comment for each built-in function to explain how they work.
+
+# print(), input(), str(), float(), range(), round(), reverse(), sum()
+print('This is a string')
+print(str('27'))
+print(int('27'))
+input('What is your favorite number: ')
+float = float(2.0)
+print(float)
