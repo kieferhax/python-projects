68cfcaa585122bdfbb66719a967e17702e9d7849
Kiefer Land <kland7@wgu.edu>
2024-07-18 10:40:09 -0700

    update distance-to-miles.py

--- python files/distance-to-miles.py
+++ python files/distance-to-miles.py
@@ -8,3 +8,8 @@
 # Take in one parameter named distance (the distance in kilometers).
 # Print the distance in miles.
 # After, call the function and use 10000 as the argument.
+def distance_to_miles(distance):
+    miles = distance / 1.609
+    print(miles)
+
+distance_to_miles(1000)
