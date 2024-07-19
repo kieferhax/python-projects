94e52164897cb7fbe8e50995a266aa0cb0520d98
Kiefer Land <kland7@wgu.edu>
2024-07-19 10:30:16 -0700

    create calculator.py

---
+++ python files/calculator.py
@@ -0,0 +1,30 @@
+# Let's try a classic Computer Science project: simple calculator program! 🔢
+# Create a calculator.py program and define these five functions:
+# add(a, b) that adds two numbers a and b.
+# subtract(a, b) that subtracts two numbers a and b
+# multiply(a, b) that multiplies two numbers a and b.
+# divide(a, b) that divides two numbers a and b.
+# exp(a, b) that takes a to the exponent (or power) of b.
+# Make sure to return the result in each function definition.
+# Test your calculator by calling each function once to make sure that it works!
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def exp(a, b):
+    return a ** b
+
+print(add(5, 5))
+print(subtract(5, 5))
+print(multiply(5, 5))
+print(divide(5, 5))
+print(exp(5, 5))
