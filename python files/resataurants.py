e259ef9a2624aa787cc638a9d659685482357e12
Kiefer Land <kland7@wgu.edu>
2024-07-23 14:09:34 -0700

    create resataurants.py

---
+++ python files/resataurants.py
@@ -0,0 +1,16 @@
+# Suppose we're building a food delivery app and want to store restaurants' info.
+
+# Create a Restaurant class in a restaurants.py file with the following attributes:
+# .name of the type str
+# .category of the type str
+# .rating of the type float
+# .delivery of the type bool
+class Restaurant:
+    name = ''
+    year = 0
+    gpa = 0.0
+    enrolled = None
+
+# Make these empty/default values for now.
+
+# Nothing should happen when you run the code, because we have yet to use this class. In the next exercise, we will start using it!
