44ede89ea3d3a2dabfd2d7e284ff5c345eae141a
Kiefer Land <kland7@wgu.edu>
2024-08-12 11:21:37 -0700

    update average-number.py

---
+++ python files/average-number.py
@@ -0,0 +1,9 @@
+# Finding an average can be a useful way to understand the underlying trends of some data.
+# Write an average() function that takes in two parameters: num1, num2
+def average(num1, num2):
+# It should calculate and return the average of the two numbers.
+    return (num1 + num2)/2
+# The average of num1 and num2 is: (num1 + num2)/2
+avg = average(2000, 2000)
+print(avg)
+# Do not use the math module
