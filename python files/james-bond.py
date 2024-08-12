6e31e892bd7e866c53a209160f0c1ff9f820879c
Kiefer Land <kland7@wgu.edu>
2024-08-12 11:06:16 -0700

    update james-bond.py

---
+++ python files/james-bond.py
@@ -0,0 +1,8 @@
+# James Bond is a classic movie character that fights bad guys and introduces himself in an iconic way: “Bond, James Bond.”
+# Write a greetings() function that takes in two parameters: first_name, last_name
+
+#  Print out the phrase with the last name, followed by a comma, and then the full name.
+
+# Next, call the function using your own name
+
+# This function doesn't have a return statement.
